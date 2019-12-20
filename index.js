4f6015e39d906f26d361d82a2311073a2de303ce
shishirgautam <gautamshishir2@gamail.com>
2019-12-20 19:28:47 +0545

    init

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 
 var app = express();
+app.use(bodyParser.urlencoded({extended:true}))
+
+app.post('/registration',function(req,res){
+    res.send('this is the Registration page')
+
+})
+app.post('/login')
+
+
+app.use('/',function(req,res){
+    res.status(404)
+    res.send('NOT FOUND');
+})
+
 app.get('/*', function(req,res){
     res.end('welcome to our new project')
 });
